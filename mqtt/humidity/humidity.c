a5d6edec3582050e1fe9a2657377ea4b537d12d6
Ricky Marinsalda <rickymarinsalda@gmail.com>
2023-05-24 18:44:24 +0200

    added humidty sensor

--- mqtt/humidity/humidity.c
+++ mqtt/humidity/humidity.c
@@ -10,14 +10,13 @@
 #include "dev/button-hal.h"
 #include "dev/leds.h"
 #include "os/sys/log.h"
-
 #include <string.h>
 #include <strings.h>
 
 #include <sys/node-id.h>
 #include <time.h>
 
-#define LOG_MODULE "temperature-sensor"
+#define LOG_MODULE "humidity"
 #ifdef  MQTT_CLIENT_CONF_LOG_LEVEL
 #define LOG_LEVEL MQTT_CLIENT_CONF_LOG_LEVEL
 #else
@@ -29,7 +28,7 @@
 
 static const char *broker_ip = MQTT_CLIENT_BROKER_IP_ADDR;
 
-// Default config values
+// Defaukt config values
 #define DEFAULT_BROKER_PORT         1883
 #define DEFAULT_PUBLISH_INTERVAL    (30 * CLOCK_SECOND)
 #define PUBLISH_INTERVAL	    (5 * CLOCK_SECOND)
@@ -46,8 +45,8 @@ static uint8_t state;
 #define STATE_SUBSCRIBED      	4	// Topics subscription done
 #define STATE_DISCONNECTED    	5	// Disconnected from MQTT broker
 
-PROCESS_NAME(process_for_temperature_sensor);
-AUTOSTART_PROCESSES(&process_for_temperature_sensor);
+PROCESS_NAME(humidity_analyzer_process);
+AUTOSTART_PROCESSES(&humidity_analyzer_process);
 
 /* Maximum TCP segment size for outgoing segments of our socket */
 #define MAX_TCP_SEGMENT_SIZE    32
@@ -77,41 +76,46 @@ static struct mqtt_message *msg_ptr = 0;
 
 static struct mqtt_connection conn;
 
-PROCESS(process_for_temperature_sensor, "Temperature sensor process");
+PROCESS(humidity_analyzer_process, "Humidity analyzer process");
 
-static bool increase_temperature = false;
-static bool decrease_temperature = false;
-static int temperature = 50; // !we cannot use float value in the testbed!
+static bool increase_humidity = false;
+static bool decrease_humidity = false;
+#define MIN_HUMIDITY 0
+#define MAX_HUMIDITY 100
+static int humidity_percentage = 50; // we cannot use float value in the testbed
 static int variation = 0;
 
 // Function called for handling an incoming message
-static void pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk, uint16_t chunk_len) {
+static void pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk, uint16_t chunk_len)
+{
 	LOG_INFO("Message received: topic='%s' (len=%u), chunk_len=%u\n", topic, topic_len, chunk_len);
 
-	if(strcmp(topic, "AC") != 0) {
+	if(strcmp(topic, "humidifier") != 0){
 		LOG_ERR("Topic not valid!\n");
 		return;
 	}
 	
 	LOG_INFO("Received Actuator command\n");
-	if(strcmp((const char*) chunk, "INC") == 0) {
-		LOG_INFO("Turn ON the heating\n");
-		increase_temperature = true;
-		decrease_temperature = false;	
-	} else if(strcmp((const char*) chunk, "DEC") == 0) {
-		LOG_INFO("Turn ON the cooler\n");	
-		increase_temperature = false;
-		decrease_temperature = true;
-	} else if(strcmp((const char*) chunk, "OFF") == 0)  {
-		LOG_INFO("Turn OFF the AC\n");	
-		increase_temperature = false;
-		decrease_temperature = false;
-	}	
+	if (strcmp((const char*) chunk, "INC") == 0){
+		LOG_INFO("Switch ON humidifier\n");
+		increase_humidity = true;
+		decrease_humidity = false;	
+	}else if (strcmp((const char*) chunk, "DEC") == 0){
+		LOG_INFO("Switch ON dehumidifier\n");	
+		increase_humidity = false;
+		decrease_humidity = true;
+	}else if (strcmp((const char*) chunk, "OFF") == 0){
+		LOG_INFO("Turn OFF humidity regulator\n");
+		increase_humidity = false;
+		decrease_humidity = false;
+	}
 }
 
 // This function is called each time occurs a MQTT event
-static void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data) {
-	switch(event) {
+static void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data)
+{
+	switch(event) 
+	{
 		case MQTT_EVENT_CONNECTED: 
 			LOG_INFO("MQTT connection acquired\n");
 			state = STATE_CONNECTED;
@@ -119,7 +123,7 @@ static void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data
 		case MQTT_EVENT_DISCONNECTED: 
 			printf("MQTT connection disconnected. Reason: %u\n", *((mqtt_event_t *)data));
 			state = STATE_DISCONNECTED;
-			process_poll(&process_for_temperature_sensor);
+			process_poll(&humidity_analyzer_process);
 			break;
 		case MQTT_EVENT_PUBLISH: 
 			msg_ptr = data;
@@ -130,7 +134,7 @@ static void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data
 			mqtt_suback_event_t *suback_event = (mqtt_suback_event_t *)data;
 			if(suback_event->success) 
 				LOG_INFO("Application has subscribed to the topic\n");
-			else
+			else 
 				LOG_ERR("Application failed to subscribe to topic (ret code %x)\n", suback_event->return_code);
 			#else
 			LOG_INFO("Application has subscribed to the topic\n");
@@ -148,16 +152,18 @@ static void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data
 	}
 }
 
-static bool have_connectivity(void) {
+static bool have_connectivity(void)
+{
 	return !(uip_ds6_get_global(ADDR_PREFERRED) == NULL || uip_ds6_defrt_choose() == NULL);
 }
 
-PROCESS_THREAD(process_for_temperature_sensor, ev, data) {
+PROCESS_THREAD(humidity_analyzer_process, ev, data)
+{
 
 	PROCESS_BEGIN();
 
-	static mqtt_status_t status;
-	static char broker_address[CONFIG_IP_ADDR_STR_LEN];
+	mqtt_status_t status;
+	char broker_address[CONFIG_IP_ADDR_STR_LEN];
 	
 	LOG_INFO("Avvio...");
 	
@@ -168,73 +174,96 @@ PROCESS_THREAD(process_for_temperature_sensor, ev, data) {
 		     linkaddr_node_addr.u8[6], linkaddr_node_addr.u8[7]);
 
 	// Broker registration					 
-	mqtt_register(&conn, &process_for_temperature_sensor, client_id, mqtt_event, MAX_TCP_SEGMENT_SIZE);
+	mqtt_register(&conn, &humidity_analyzer_process, client_id, mqtt_event, MAX_TCP_SEGMENT_SIZE);
 			
 	state=STATE_INIT;
 				    
 	// Initialize periodic timer to check the status 
 	etimer_set(&periodic_timer, PUBLISH_INTERVAL);
 
-	while(true) {
+	while(true) 
+	{
 		PROCESS_YIELD();
 
-		if(!((ev == PROCESS_EVENT_TIMER && data == &periodic_timer) || ev == PROCESS_EVENT_POLL))
-			continue;
-		
-		switch(state) {		  			  
-		case STATE_INIT:
-			LOG_INFO("Init....");
-			if(have_connectivity())
-				state = STATE_NET_OK;
-			else
-				break;
-		// no break
-		case STATE_NET_OK:
-			LOG_INFO("Connecting to MQTT server\n"); 
-		  	memcpy(broker_address, broker_ip, strlen(broker_ip));
-		  
-		  	mqtt_connect(&conn, broker_address, DEFAULT_BROKER_PORT,
-					   (DEFAULT_PUBLISH_INTERVAL * 3) / CLOCK_SECOND,
-					   MQTT_CLEAN_SESSION_ON);
-		  	state = STATE_CONNECTING;
-		break;
-		case STATE_CONNECTED:
+		if((ev == PROCESS_EVENT_TIMER && data == &periodic_timer) || ev == PROCESS_EVENT_POLL)
+		{	
+			switch(state){
+			case STATE_INIT:
+				LOG_INFO("state init..");
+				if(have_connectivity())
+					state = STATE_NET_OK;
+				else
+					break;
+			// no break
+			case STATE_NET_OK:
+				LOG_INFO("Connecting to MQTT server\n"); 
+			  	memcpy(broker_address, broker_ip, strlen(broker_ip));
+			  
+			  	mqtt_connect(&conn, broker_address, DEFAULT_BROKER_PORT,
+						   (DEFAULT_PUBLISH_INTERVAL * 3) / CLOCK_SECOND,
+						   MQTT_CLEAN_SESSION_ON);
+			  	state = STATE_CONNECTING;
+			break;
+			case STATE_CONNECTED:
 			// Subscribe to a topic
-			strcpy(sub_topic,"AC");
+			strcpy(sub_topic,"humidifier");
 			status = mqtt_subscribe(&conn, NULL, sub_topic, MQTT_QOS_LEVEL_0);
 			if(status == MQTT_STATUS_OUT_QUEUE_FULL) {
 				LOG_ERR("Tried to subscribe but command queue was full!\n");
 				PROCESS_EXIT();
 			}
 			state = STATE_SUBSCRIBED;
-		// no break
-		case STATE_SUBSCRIBED:	
-			sprintf(pub_topic, "%s", "temperature");
+			// no break
+			case STATE_SUBSCRIBED:	
+			sprintf(pub_topic, "%s", "humidity");
 			
 			// simulate the behavior of the sensor				
-			if (increase_temperature || decrease_temperature) {
-				variation = rand()%3; 	// a value in [0,2]
-				temperature = (increase_temperature) ? (temperature + variation) : (temperature - variation);
-			} else {
-				if((rand()%10) < 6) { // 60% chance that the temperature will change
-					variation = (rand()%5)-2; // a value in [-2, 2]
-					temperature = temperature + variation;
+			if (increase_humidity || decrease_humidity)
+				{
+					variation = (rand()%10)+1; 	// a value in [1,10]
+					if (increase_humidity)
+					{
+						humidity_percentage = humidity_percentage + variation;
+					}
+					else
+					{
+						humidity_percentage = humidity_percentage - variation;
+					}
 				}
-			}
+				else // humidity regulator OFF
+				{
+					// compute a probability to have a change
+					if ((rand()%10) < 6) // 60% chance that the humidity will change
+					{
+						variation = (rand()%9)-4; // a value in [-4,4];
+						humidity_percentage = humidity_percentage + variation;
+					}
+				}			
 
-			LOG_INFO("New value of temperature: %d\n", temperature);
+				if (humidity_percentage > MAX_HUMIDITY) // impossible behavior in a real environment
+				{
+					humidity_percentage = MAX_HUMIDITY; 
+				}
+				else if (humidity_percentage < MIN_HUMIDITY) // impossible behavior in a real environment
+				{
+					humidity_percentage = MIN_HUMIDITY;
+				}
+
+				LOG_INFO("New value of humidity: %d%%\n", humidity_percentage);
+				
+				sprintf(app_buffer, "{\"node\": %d, \"humidity\": %d}", node_id, humidity_percentage);
+				mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer,
+				strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
+
+			break; 
 			
-			sprintf(app_buffer, "{\"node\": %d, \"temperature\": %d}", node_id, temperature);
-			mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer,
-			strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
-
-		break; 
-		case STATE_DISCONNECTED:
+			case STATE_DISCONNECTED:
 			LOG_ERR("Disconnected from MQTT broker\n");	
 			state = STATE_INIT;
-		break;
-		}
-		etimer_set(&periodic_timer, PUBLISH_INTERVAL);
-	}
-	PROCESS_END();
+			break;
+			}
+			etimer_set(&periodic_timer, PUBLISH_INTERVAL);
+			}
+			PROCESS_END();
+
 }
