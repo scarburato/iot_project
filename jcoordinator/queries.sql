046cf96258d8c3b94ca55bd9bfed6be72a316a52
Dario Pagani <dario.pagani.146@gmail.com>
2023-05-29 15:09:06 +0200

    db driver

---
+++ jcoordinator/queries.sql
@@ -0,0 +1,7 @@
+INSERT INTO temperature (`sensor's id`, degrees) VALUES (?, ?);
+
+INSERT INTO humidity (`sensor's id`, percentage) VALUES (?, ?);
+
+INSERT INTO `float level`(`sensor's id`, `low level`) VALUES (?, ?);
+
+INSERT INTO `air quality`(`sensor's id`, concentration) VALUES (?, ?);
