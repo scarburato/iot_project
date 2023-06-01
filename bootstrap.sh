17d9bba05b990a5f075ff60f9cb1b8a762959b19
Dario Pagani <dario.pagani.146@gmail.com>
2023-06-01 17:09:27 +0200

    fixed script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,5 @@ gnome-terminal \
 	--tab-with-profile=IoT --title="cooja" -e "docker start -ai $CONTAINER_NAME" \
 	--tab-with-profile=IoT --title="mosquitto" -e "docker exec -it $CONTAINER_NAME bash -c 'mosquitto -v -c ~/project/mosquitto.conf'" \
 	--tab-with-profile=IoT --title="bridge" -e "docker exec -it $CONTAINER_NAME bash -c 'cd ~/contiki-ng/examples/rpl-border-router; bash'" \
-	--tab-with-profile=IoT --title="server" -e "docker exec -it $CONTAINER_NAME bash -c 'cd ~/project/CoAP\ server/target; bash'" \
+	--tab-with-profile=IoT --title="server" -e "docker exec -it $CONTAINER_NAME bash -c 'cd ~/project/jcoordinator/target; bash'" \
 	--tab-with-profile=IoT --title="altra roba" -e "docker exec -it $CONTAINER_NAME bash -c 'cd ~/project; bash'"
